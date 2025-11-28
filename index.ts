f38959f98aeeb17b8543744083f47bd5585f290b
Yuqian Li <liyuqian79@gmail.com>
2025-11-28 11:46:47 -0800

    Generate points

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
-import {evaluate, norm, subtract, add, cross, dotDivide} from 'mathjs';
+import {add, cross, distance, dotDivide, evaluate, norm, subtract} from 'mathjs';
 
 type Vec2 = [number, number];
 type Vec3 = [number, number, number];
+
+const kDrawFocals = false as const;
+const kDrawSegments = false as const;
 
 function main() {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -43,6 +46,7 @@ class Polycurve {
     }
     pg1.push(pg1.at(-1)!);
     for (let i = pg1.length - 1; i > 0; i--) {
+      // Original pg1[i] used i and i+1. Now average with i - 1 for symmetry.
       pg1[i] = normalize(add(pg1[i], pg1[i - 1]));
     }
     this.point_gradients.set(0, pg0);
@@ -62,47 +66,64 @@ class Polycurve {
     return this.point_gradients.get(0)!.length;
   }
 
+  // Original sample points given to the constructor as input.
   public point(i: number): Vec2 {
     return this.pg(0, i);
+  }
+
+  // Generate an arbitrary point on the curve with an optional leftOffset.
+  //
+  // The generalI can be fractional as we support interpolation/extrapolation.
+  public generatePoint(generalI: number, leftOffset: number = 0): Vec2 {
+    if (this.n == 0) return [0, 0];
+    if (this.n == 1) return this.pg(0, 0);
+
+    // Clamp to n-2 because focal points have n - 1 indices from 0 to n-2.
+    const i = clamp(Math.floor(generalI), 0, this.n - 2);
+    const reminder = generalI - i;
+    const focal = this.focal(i);
+    const p = this.pg(0, i);
+    const q = this.pg(0, i + 1);
+
+    const lerped: Vec2 =
+        evaluate('p * (1 - r) + q * r', {p: p, q: q, r: reminder});
+
+    if (Math.abs(focal[2]) <= Polycurve.kEps) {
+      return evaluate(
+          'p + l * d', {p: p, l: this.left(i), d: leftOffset});
+    }
+
+    const f: Vec2 = fromHomo(focal);
+    const left = normalize(subtract(lerped, f));
+    const dLerped = evaluate(
+        'dp * (1 - r) + dq * r',
+        {dp: distance(p, f), dq: distance(q, f), r: reminder});
+    return evaluate('f + l * d', {f: f, l: left, d: dLerped});
   }
 
   public focal(i: number): Vec3 {
     return this.focals[i];
   }
 
-  private pg(i: number, j: number): Vec2 {
-    return this.point_gradients.get(i)![j];
+  private pg(order: number, i: number): Vec2 {
+    return this.point_gradients.get(order)![i];
   }
-
   private left(i: number): Vec2 {
     const tangent = this.pg(1, i);
     return [-tangent[1], tangent[0]];
   }
-
-
-  private static filterDuplicates(points: Vec2[]): Vec2[] {
-    if (points.length == 0) return [];
-    const filtered: Vec2[] = [];
-    filtered.push(points[0]);
-    for (let i = 1; i < points.length; i++) {
-      const delta: Vec2 = subtract(points[i], filtered.at(-1)!);
-      if (norm(delta) as number > Polycurve.kEps) {
-        filtered.push(points[i]);
-      }
-    }
-    return filtered;
-  }
 }
 
+function clamp(x: number, min: number, max: number): number {
+  return Math.max(min, Math.min(x, max));
+}
 function normalize(v: Vec2): Vec2 {
   const len = norm(v) as number;
   return dotDivide(v, len == 0 ? 1 : len);
 }
-
 function toHomo(xy: Vec2): Vec3 {
   return [xy[0], xy[1], 1];
 }
-
 function fromHomo(h: Vec3): Vec2 {
   return [h[0] / h[2], h[1] / h[2]];
 }
@@ -140,14 +161,26 @@ function drawLine(
 }
 
 function drawPolycurve(ctx: CanvasRenderingContext2D, polycurve: Polycurve) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   for (let i = 0; i < polycurve.n; i++) {
     drawPoint(ctx, polycurve.point(i));
   }
   for (let i = 0; i < polycurve.n - 1; i++) {
-    drawLine(ctx, polycurve.point(i), polycurve.point(i + 1));
-    drawPoint(ctx, fromHomo(polycurve.focal(i)), 'red');
-    drawLine(ctx, polycurve.point(i), fromHomo(polycurve.focal(i)), 'red');
-    drawLine(ctx, polycurve.point(i + 1), fromHomo(polycurve.focal(i)), 'red');
+    if (kDrawSegments) {
+      drawLine(ctx, polycurve.point(i), polycurve.point(i + 1));
+    }
+    if (kDrawFocals) {
+      drawPoint(ctx, fromHomo(polycurve.focal(i)), 'red');
+      drawLine(ctx, polycurve.point(i), fromHomo(polycurve.focal(i)), 'red');
+      drawLine(ctx, polycurve.point(i + 1), fromHomo(polycurve.focal(i)), 'red');
+    }
+  }
+  const kSampleCount = 1000 as const;
+  let prev = polycurve.generatePoint(0);
+  for (let i = 1; i < kSampleCount; i++) {
+    const point = polycurve.generatePoint(i / kSampleCount * (polycurve.n - 1));
+    drawLine(ctx, prev, point);
+    prev = point;
   }
 }
 
